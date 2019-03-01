3f9789b76e488e691a7215fe4a5de125465193b7
Johannes <j.rosvik@gmail.com>
2019-03-01 16:28:10 +0100

    #6 Added base64 encoding and decoding

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ let input = document.getElementById('hash_entry');
 load_params();
 full_compute(input.value);
 
-input.onkeyup = function() { 
-	full_compute(input.value); 
+input.onkeyup = function () {
+	full_compute(input.value);
 }
 
 // Use URL parameters if set
@@ -25,15 +25,38 @@ function full_compute(input) {
 	js_sha(input, 'SHA3-384', 'sha3384_out');
 	js_sha(input, 'SHA-512', 'sha512_out');
 	js_sha(input, 'SHA3-512', 'sha3512_out');
+	b64(input, false, 'b64e_out');
+	b64(input, true, 'b64d_out');
 
 	document.getElementById('chars').innerHTML = chars_info(input);
+}
+
+function setOutVal(id, value) {
+	document.getElementById(id).textContent = value;
+}
+
+// Do base-64 encoding/decoding
+function b64(input, decode, id) {
+	if (!decode) { // Encode
+		try {
+			setOutVal(id, btoa(input));
+		} catch (e) {
+			setOutVal(id, "Incompatible string");
+		}
+	} else { // Decode
+		try {
+			setOutVal(id, atob(input));
+		} catch (e) {
+			setOutVal(id, "Incompatible string");
+		}
+	}
 }
 
 // Hashes from js-crypto
 function compute(input, hash, id) {
 	let entry_value = input;
 	let hash_value = hash(entry_value);
-	document.getElementById(id).textContent = hash_value;
+	setOutVal(id, hash_value);
 }
 
 // Hashes from jsSHA
@@ -41,7 +64,7 @@ function js_sha(input, hash, id) {
 	var shaObj = new jsSHA(hash, 'TEXT');
 	shaObj.update(input);
 	var hash = shaObj.getHash('HEX');
-	document.getElementById(id).textContent = hash;
+	setOutVal(id, hash);
 }
 
 // Compute and display character information
@@ -52,7 +75,7 @@ function chars_info(input) {
 	let stringarr = Array.from(input);
 
 	if (stringarr.length > 0) {
-		out += "<tr>" 
+		out += "<tr>"
 			+ "<th>Letter</th>"
 			+ "<th>Unicode</th>"
 			+ "<th>Dec / Hex</th>"
@@ -65,7 +88,7 @@ function chars_info(input) {
 		let hex_code = code.toString(16).toUpperCase();
 		let unicode = "U+" + hex_code.padStart(4, '0');
 
-		out += "<tr>" 
+		out += "<tr>"
 			+ "<td><span class='char'><b>" + letter + "</b></span></td>"
 			+ "<td><a href='https://www.compart.com/en/unicode/" + unicode + "'>" + unicode + "</a></td>"
 			+ "<td>" + code + " / 0x" + hex_code + "</td>"
